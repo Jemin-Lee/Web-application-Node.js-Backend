3e6d874ed81f425be911420381d2d2ce2687b7aa
Jemin Lee <jle147@uclive.ac.nz>
2020-03-26 23:01:52 +1300

    add petition update 5

--- app/controllers/petitions.controller.js
+++ app/controllers/petitions.controller.js
@@ -1,4 +1,15 @@
 const petitionsModel = require('../models/petitions.model');
+
+function checkEmpty(input){
+    if (!input){
+        return false;
+    }else{
+        //input empty || only white-space || null
+        return !(input.length === 0 || !input.trim() || !input);
+    }
+}
+
+
 /*list all petitions come back later
 
 
@@ -13,8 +24,8 @@ exports.viewPetitions = async function (req, res){
 exports.addPetition = async function (req, res) {
   const categoriesDB = await petitionsModel.categories();
   const categories = categoriesDB[0];
-  let reqTitle = req.body.title;
-  if (reqTitle.length == 0 || !reqTitle.trim() || !reqTitle.title){
+
+  if (((!('title' in req.body)) || (!checkEmpty(req.body.title)))){
     res.statusMessage = "Bad Request title";
     res.status(400).send();
   }
