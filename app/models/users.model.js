87e066430e7eb02da946ff2091967fb52ad3c557
Jemin Lee <jle147@uclive.ac.nz>
2020-04-04 02:34:13 +1300

    photo last

--- app/models/users.model.js
+++ app/models/users.model.js
@@ -212,8 +212,11 @@ exports.deleteProfilePhoto = async function (photo, currentId){
   try{
     const query = `update User set photo_filename = null where user_id = ?`;
     const conn = await db.getPool().getConnection();
-    await conn.query(query, currentId);
+    const result = await conn.query(query, currentId);
     conn.release();
+    if (result.changedRows !== 1) {
+            throw Error('Should be exactly one user whose profile photo was modified.');
+        }
     if (await fs.exists('./storage/default/' + photo)){
       await fs.unlink('./storage/default/' + photo);
     }
