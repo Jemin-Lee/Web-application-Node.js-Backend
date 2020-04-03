65804b44a36f8b3ffc6333473ecc2747a08122dc
Jemin Lee <jle147@uclive.ac.nz>
2020-04-04 04:26:49 +1300

    test

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
     //app.route(app.rootUrl + '/users/:id').patch(authentication.setToken, usersController.changeDetails);
 
     app.route(app.rootUrl + '/users/:id/photo').get(usersController.getProfilePhoto);
-    //app.route(app.rootUrl + '/users/:id/photo').delete(authentication.setToken, usersController.deleteProfilePhoto);
+    app.route(app.rootUrl + '/users/:id/photo').delete(authentication.setToken, usersController.deleteProfilePhoto);
     app.route(app.rootUrl + '/users/:id/photo').put(authentication.setToken, usersController.setProfilePhoto);
 
 
