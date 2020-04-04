d89d2285c0cb55b799451518bb0487f354590b8b
Jemin Lee <jle147@uclive.ac.nz>
2020-04-05 05:53:27 +1200

    this aint for me

--- app/routes/users.photo.routes.js
+++ app/routes/users.photo.routes.js
@@ -2,7 +2,7 @@ const usersController = require('../controllers/users.controller');
 const authentication = require('../middleware/authentication');
 
 module.exports = function (app) {
-  app.route(app.rootUrl + '/users/:id/photo').get(usersController.getProfilePhoto)
+  app.route(app.rootUrl + '/users/:id/photo').get(usersController.getProfilePhoto);
   //.delete(authentication.userLoginCheck, usersController.deleteProfilePhoto)
-  //.put(authentication.userLoginCheck, usersController.setProfilePhoto);
+  app.route(app.rootUrl + '/users/:id/photo').put(authentication.userLoginCheck, usersController.setProfilePhoto);
 };
