b940ecf79fb1082d753f3b336a11fd54fe4eca06
Jemin Lee <jle147@uclive.ac.nz>
2020-04-04 18:47:20 +1300

    test

--- app/middleware/authentication.js
+++ app/middleware/authentication.js
@@ -3,13 +3,18 @@ const userModel =  require('../models/users.model');
 exports.setToken = async function (req, res, next) {
   const userToken = req.header('X-Authorization');
   try {
-    const foundUserList = await userModel.findUserToken(userToken);
-    if (foundUserList !== null) {
-      req.currentId = foundUserList[0].user_id.toString();
-    } else{
-      req.currentId = null;
+    if (!userToken){
+      res.statusMessage = "Unauthorized no token";
+      res.status(401).send();
+    }else{
+      const foundUserList = await userModel.findUserToken(userToken);
+      if (foundUserList !== null) {
+        req.currentId = foundUserList[0].user_id.toString();
+      } else{
+        req.currentId = null;
+      }
+      next();
     }
-    next();
   }catch(err){
       res.status(500).send();
   }
