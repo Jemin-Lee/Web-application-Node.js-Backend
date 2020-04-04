3652e627803acd05144b3feb3d264af01a01d0b7
Jemin Lee <jle147@uclive.ac.nz>
2020-04-04 18:58:59 +1300

    test

--- app/middleware/authentication.js
+++ app/middleware/authentication.js
@@ -3,10 +3,10 @@ const userModel =  require('../models/users.model');
 exports.setToken = async function (req, res, next) {
   const userToken = req.header('X-Authorization');
   try {
-    if (!userToken){
-      res.statusMessage = "Unauthorized no token";
-      res.status(401).send();
-    }else{
+    // if (!userToken){
+    //   res.statusMessage = "Unauthorized no token";
+    //   res.status(401).send();
+    // }else{
       const foundUserList = await userModel.findUserToken(userToken);
       if (foundUserList !== null) {
         req.currentId = foundUserList[0].user_id.toString();
@@ -14,7 +14,7 @@ exports.setToken = async function (req, res, next) {
         req.currentId = null;
       }
       next();
-    }
+    // }
   }catch(err){
       res.status(500).send();
   }
