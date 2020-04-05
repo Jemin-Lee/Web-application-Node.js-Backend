51b77dca9913337fb8f8be90070d4a2993241e7c
Jemin Lee <jle147@uclive.ac.nz>
2020-04-05 23:27:59 +1200

    this aint for me

--- app/routes/petitions.routes.js
+++ app/routes/petitions.routes.js
@@ -2,13 +2,16 @@ const petitionsController = require('../controllers/petitions.controller');
 const authentication = require('../middleware/authentication');
 
 module.exports = function (app) {
-  app.route(app.rootUrl + '/petitions').post(authentication.userLoginCheck, petitionsController.addPetition);
+  app.route(app.rootUrl + '/petitions')
+  .post(authentication.userLoginCheck, petitionsController.addPetition);
 
   app.route(app.rootUrl + '/petitions/categories').get(petitionsController.viewCategories);
 
-  app.route(app.rootUrl + '/petitions/:id')//.get(petitionsController.viewPetition)
+  app.route(app.rootUrl + '/petitions/:id')
+  .get(petitionsController.viewPetition)
   .patch(authentication.setToken, petitionsController.changePetition)
   .delete(authentication.setToken, petitionsController.deletePetition);
 
-  app.route(app.rootUrl + '/petitions/:id/photo').get(petitionsController.viewPetitionPhoto);
+  app.route(app.rootUrl + '/petitions/:id/photo')
+  .get(petitionsController.viewPetitionPhoto);
 };
